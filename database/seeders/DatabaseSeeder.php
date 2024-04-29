20716b7703439db6236160376c98562bbcc480d2
mohamed1maghraby-div <mohamed1maghraby@yahoo.com>
2024-04-29 23:44:02 +0300

    add migrations and models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Conversation;
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Jahn Doe',
+            'email' => 'jahn@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(10)->create();
+
+        for($i=0; $i<5; $i++){
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+
+        $conversations = $messages->groupBy(function($message){
+            return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
+        })->map(function($groupedMessages){
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id
+            ];
+        })->values();
+        
+        Conversation::insertOrIgnore($conversations->toArray());
     }
 }
